735160a023350bce4c6aba5d2c47976b9004982a
Guhan2348519 <139948192+Guhan2348519@users.noreply.github.com>
2023-11-18 11:37:14 +0530

    Update AlphabetWar.java

--- AlphabetWar.java
+++ AlphabetWar.java
@@ -37,71 +37,70 @@
 
 import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class AlphabetWar {
-private String left_side_letters = "wpbs";
-private String right_side_letters = "mqdz";
-private int strength_W = 4;
-private int strength_P = 3;
-private int strength_B = 2;
-private int strength_S = 1;
-private int strength_M = 4;
-private int strength_Q = 3;
-private  int strength_D = 2;
-private  int strength_Z = 1;
- public static void main(String[] args) {
- Scanner scanner = new Scanner(System.in);
-System.out.print("Enter a word: ");
-String userIn = scanner.nextLine();
-System.out.println(AlphabetWar(userInput));
- scanner.close();
-}
-public String AlphabetWar(String word) {
-int leftStrength = 0;
-int rightStrength = 0;
- for (char letter : word.toCharArray()) {
-  if (left_side_letters.contains(String.valueOf(letter))) {
-leftStrength += LeftSideStrength(letter);
-  } else if (right_side_letters.contains(String.valueOf(letter))) {
-rightStrength += RightSideStrength(letter);
- } else {
- return handleInvalid(letter);
- }}
-if (leftStrength > rightStrength) {
-  return "Left side wins!";
-  } else if (rightStrength > leftStrength) {
-   return "Right side wins!";
-  } else {
-  return "Let's fight again!";
-   }}
-private static int LeftSideStrength(char letter) {
-  switch (letter) {
-case 'w':
- return strength_W;
-  case 'p':
-  return strength_P;
- case 'b':
- return strength_B;
- case 's':
- return strength_S;
-  default:
- return 0; // Invalid letter
+    private static String left_side_letters = "wpbs";
+    private static String right_side_letters = "mqdz";
+    private static int strength_W = 4;
+    private static int strength_P = 3;
+    private static int strength_B = 2;
+    private static int strength_S = 1;
+    private static int strength_M = 4;
+    private static int strength_Q = 3;
+    private static int strength_D = 2;
+    private static int strength_Z = 1;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String userInput = scanner.nextLine();
+        System.out.println(AlphabetWar(userInput));
+        scanner.close();
     }
+
+    public static String AlphabetWar(String word) {
+        int leftStrength = 0;
+        int rightStrength = 0;
+        for (char letter : word.toCharArray()) {
+            if (left_side_letters.contains(String.valueOf(letter))) {
+                leftStrength += LeftSideStrength(letter);
+            } else if (right_side_letters.contains(String.valueOf(letter))) {
+                rightStrength += RightSideStrength(letter);
+            } else {
+                return handleInvalid(letter);
+            }
+        }
+        if (leftStrength > rightStrength) {
+            return "Left side wins!";
+        } else if (rightStrength > leftStrength) {
+            return "Right side wins!";
+        } else {
+            return "Let's fight again!";
+        }
     }
-private static int RightSideStrength(char letter) {
- switch (letter) {
-case 'm':
-   return strength_M;
-case 'q':
- return strength_Q;
-case 'd':
- return strength_D;
- case 'z':
- return strength_Z;
-  default:
-return 0; // Invalid letter
- }
-  }
-  private static String handleInvalid(char letter) 
-{
+
+    private static int LeftSideStrength(char letter) {
+        switch(letter) {
+            case 'w': return strength_W;
+            case 'p': return strength_P;
+            case 'b': return strength_B;
+            case 's': return strength_S;
+            default: return 0; // Invalid letter
+        }
+    }
+
+    private static int RightSideStrength(char letter) {
+        switch (letter) {
+            case 'm': return strength_M;
+            case 'q': return strength_Q;
+            case 'd': return strength_D;
+            case 'z': return strength_Z;
+            default: return 0; // Invalid letter
+        }
+    }
+
+    private static String handleInvalid(char letter) {
         return "Invalid letter: " + letter;
- }}
+    }
+}
